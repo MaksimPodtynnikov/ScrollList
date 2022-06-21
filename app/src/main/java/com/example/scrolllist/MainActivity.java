aff45db7b3a99beb40537d96c1bc21232cfc4336
Maksim Podtynnikov <mpodtynnikov@mail.ru>
2022-06-21 14:08:49 +0300

    update titles of ListView and list of textmarks

--- app/src/main/java/com/example/scrolllist/MainActivity.java
+++ app/src/main/java/com/example/scrolllist/MainActivity.java
@@ -8,15 +8,15 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] texts = { "text01", "text02", "text03", "text04", "text05","text06","text07","text08","text09","text010","text011","text012","text013","text014"};
+    String[] textMarks = { "text01", "text02", "text03", "text04", "text05","text06","text07","text08","text09","text010","text011","text012","text013","text014"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView countriesList = findViewById(R.id.textList);
+        ListView countriesList = findViewById(R.id.marksList);
         ArrayAdapter<String> adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, texts);
+                android.R.layout.simple_list_item_1, textMarks);
         countriesList.setAdapter(adapter);
     }
 }
