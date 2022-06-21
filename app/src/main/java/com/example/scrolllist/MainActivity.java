e4474251426847324fa7034c81b3aa5a2c51b1b5
Maksim Podtynnikov <mpodtynnikov@mail.ru>
2022-06-21 14:04:17 +0300

    update textmarks

--- app/src/main/java/com/example/scrolllist/MainActivity.java
+++ app/src/main/java/com/example/scrolllist/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] texts = { "text1", "text2", "text3", "text5", "text5","text6","text7","text8","text9","text10","text11","text12","text13","text14"};
+    String[] texts = { "text01", "text02", "text03", "text04", "text05","text06","text07","text08","text09","text010","text011","text012","text013","text014"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
